3496d65a2c234596da8e8c13ab696690120281be
DM <dmoosocool@gmail.com>
2022-04-29 20:29:17 +0800

    feat: support unocss

--- test/Home.test.ts
+++ test/Home.test.ts
@@ -1,29 +1,30 @@
-import { shallowMount } from '@vue/test-utils';
-import { expect, it, describe, beforeAll } from 'vitest';
-import { createPinia } from 'pinia';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { shallowMount } from '@vue/test-utils'
+import { expect, it, describe, beforeAll } from 'vitest'
+import { createPinia } from 'pinia'
 
-import Home from '@/views/home';
+import Home from '@/views/home'
 
-let wrapper;
+let wrapper: any
 beforeAll(() => {
   wrapper = shallowMount(Home, {
     global: {
       plugins: [createPinia()],
     },
-  });
-});
+  })
+})
 
 describe('tsx vue pinia', () => {
   it('test mounted', () => {
-    expect(wrapper.html()).toMatchSnapshot();
-  });
+    expect(wrapper.html()).toMatchSnapshot()
+  })
 
   it('test events and pinia', async () => {
-    const plusBtn = wrapper.find('button.plus-btn');
-    const minusBtn = wrapper.find('button.minus-btn');
-    await plusBtn.trigger('click');
-    expect(wrapper.html()).toContain('<p>your counter: 1</p>');
-    await minusBtn.trigger('click');
-    expect(wrapper.html()).toContain('<p>your counter: 0</p>');
-  });
-});
+    const plusBtn = wrapper.find('button.plus-btn')
+    const minusBtn = wrapper.find('button.minus-btn')
+    await plusBtn.trigger('click')
+    expect(wrapper.html()).toContain('<p>your counter: 1</p>')
+    await minusBtn.trigger('click')
+    expect(wrapper.html()).toContain('<p>your counter: 0</p>')
+  })
+})
